216c93b94005a4884a367bd20aa22531307e3553
Ayodele Akinbohun <daylay92@yahoo.com>
2023-08-17 21:25:41 +0100

    passing commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.status(400).json({});
+  res.status(200).json({});
 });
 app.listen(3000, () => {
     console.log('App cracking on 3000!!!')
